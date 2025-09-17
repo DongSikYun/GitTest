48a0a3cb6ad03efb99af363c675ed95c5da6a83a
윤동식 <yds0903@kumc.or.kr>
2025-09-17 15:23:16 +0900

    master add

--- GitTest/src/com/git/test/GitTest.java
+++ GitTest/src/com/git/test/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("FirstCommit");
 		System.out.println("SecondCommit");
+		System.out.println("master commit");
 		System.out.println("testcommit");
 		System.out.println("test2commit");
 	}
