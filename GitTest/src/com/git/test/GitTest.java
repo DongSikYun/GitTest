9aa765670709034e7a41bb36015cfac45970f722
윤동식 <yds0903@kumc.or.kr>
2025-09-17 15:21:44 +0900

    testcommit

--- GitTest/src/com/git/test/GitTest.java
+++ GitTest/src/com/git/test/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("FirstCommit");
 		System.out.println("SecondCommit");
+		System.out.println("testcommit");
 	}
 }
