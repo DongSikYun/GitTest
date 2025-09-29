055438874ea4957f08dfce52eb5e77a213df7848
윤동식 <yds0903@kumc.or.kr>
2025-09-29 10:05:23 +0900

    test

--- GitTest/src/com/git/test/GitTest.java
+++ GitTest/src/com/git/test/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
 		System.out.println("test3commit");
 		System.out.println("test3commit");
 		System.out.println("test3commit");
+		System.out.println("test3commit");
 	}
 }
